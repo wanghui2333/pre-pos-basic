ecbc71e15768379a5827f0e70c2f3ce67aaf54a3
wanghui <793147654@qq.com>
2019-08-21 10:18:37 +0800

    完成查找元素代码

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,12 @@
 'use strict';
 
-function find(collection, element) {
-  return '实现练习要求，并改写该行代码。';
+function find(collection, element) {  
+
+  let ret;
+  for (let i = 0; i < collection.length; i++) {
+    if(collection[i] === element){
+      return collection[i];
+    }
+  }
+  return '没有该元素';
 }
